4e201d23f3ab6dcb9b7d96ecd74e2a94eb11cc04
Jung Jihyeong <wlgud3936@gmail.com>
2023-11-01 04:14:37 +0900

    초기설정

---
+++ OpenCV/build/intermediates/cxx/Debug/b2jpd3z5/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\dwkm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\dwkm\\AndroidStudioProjects\\opencvproject\\OpenCV\\.cxx\\Debug\\b2jpd3z5\\x86_64" ^
+  opencv_jni_shared
---
+++ app/src/main/java/com/example/opencvproject/MainActivity.kt
@@ -0,0 +1,20 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.opencvproject.R
+import org.opencv.android.OpenCVLoader
+
+private const val TAG = "TEST_OPEN_CV_ANDROID"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(TAG, "OpenCV 초기화 실패!")
+        } else {
+            Log.d(TAG, "OpenCV 초기화 성공!!!!!")
+        }
+    }
+}
