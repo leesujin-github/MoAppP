b4c5a6d6e82b925ef724e9976af18a6208e39586
Jung Jihyeong <wlgud3936@gmail.com>
2023-11-06 14:47:36 +0900

    수정

--- OpenCV/build/intermediates/cxx/Debug/b2jpd3z5/logs/x86/configure_command.bat
+++ OpenCV/build/intermediates/cxx/Debug/b2jpd3z5/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\dwkm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\dwkm\\AndroidStudioProjects\\opencv_1\\OpenCV\\libcxx_helper" ^
+  "-HC:\\Users\\dwkm\\AndroidStudioProjects\\opencvproject\\OpenCV\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\dwkm\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\dwkm\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\dwkm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\dwkm\\AndroidStudioProjects\\opencv_1\\OpenCV\\build\\intermediates\\cxx\\Debug\\b2jpd3z5\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\dwkm\\AndroidStudioProjects\\opencv_1\\OpenCV\\build\\intermediates\\cxx\\Debug\\b2jpd3z5\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\dwkm\\AndroidStudioProjects\\opencvproject\\OpenCV\\build\\intermediates\\cxx\\Debug\\b2jpd3z5\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\dwkm\\AndroidStudioProjects\\opencvproject\\OpenCV\\build\\intermediates\\cxx\\Debug\\b2jpd3z5\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\dwkm\\AndroidStudioProjects\\opencv_1\\OpenCV\\.cxx\\Debug\\b2jpd3z5\\x86" ^
+  "-BC:\\Users\\dwkm\\AndroidStudioProjects\\opencvproject\\OpenCV\\.cxx\\Debug\\b2jpd3z5\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
